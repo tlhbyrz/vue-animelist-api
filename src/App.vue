17d05afc0bb36e18a0242606f144da50cd3b1d7a
tlhbyrz <talhaboyraz13@gmail.com>
2021-04-03 20:52:58 +0300

    uygulamanın tasarımı tamamlandı.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,112 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <header>
+      <h1>The <strong>Anime</strong> Api</h1>
+      <form class="search-form">
+        <input 
+          type="search"
+          class="search-input"
+          placeholder="Search for an anime" 
+          required>
+      </form>
+    </header>
+
+    <main>
+      <div class="cards">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from "./components/Card"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Card
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Livvic:wght@400;500;600;700;900&display=swap');
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body{
+  line-height: 1.5;
+  font-family: 'Livvic', sans-serif;
+}
+
+.container{
+  width: 100%;
+  max-width: 660px;
+  margin: 0 auto;
+}
+
+header{
+  margin: 50px 0 50px 0;
+
+  h1{
+    color: #888;
+    text-align: center;
+    font-size: 40px;
+    text-transform: uppercase;
+
+    strong{
+      color: #313131;
+    }
+  }
+
+  .search-form{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    padding: 0 30px;
+    margin-top: 30px;
+
+    .search-input{
+      display: block;
+      width: 100%;
+      max-width: 600px;
+      padding: 15px;
+      border-radius: 8px;
+      font-size: 20px;
+      font-weight: 600;
+      box-shadow: 0px 4px 8px rgba($color: #000000, $alpha: .15);
+      border: none;
+      outline: none;
+      appearance: none;
+      background: #FAFAFA;
+
+      &::placeholder{
+        color: #AAA;
+      }
+
+      &:focus, &:valid{
+        background: #313131;
+        color: #fff;
+      }
+    }
+  }
+}
+
+main{
+  padding: 15px 30px;
+
+  .cards{
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0 -10px;
+  }
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card">
+      <img class="card-img" src="https://place-hold.it/150x250" alt="Naruto img">
+      <h3>Naruto</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Card"
+}
+</script>
+
+<style lang="scss" scoped>
+.card{
+    width: 100%;
+    max-width: calc(100% / 3);
+    padding: 10px;
+    margin-top: 10px;
+    cursor: pointer;
+
+    .card-img{
+        width: 100%;
+        border-radius: 10px;
+        transition: .4s;
+    }
+
+    h3{
+        padding: 5px 10px;
+        text-align: center;
+    }
+
+    &:hover{
+        .card-img{
+            transform: scale(1.05);
+        }
+    }
+}
+
+@media screen and (max-width: 500px) {
+    .card{
+       max-width: calc(100% / 2); 
+    }        
+}
+</style>
